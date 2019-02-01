f3c51b4ad6ff6f53ef18ca28a7777b20345050c1
David Braley <david.braley@gmail.com>
2019-02-01 12:44:08 -0500

    Refactor to get rid of early returns

--- state-machine.js
+++ state-machine.js
@@ -12,15 +12,11 @@ class StateMachine {
   read(commands) {
     if (this.state === STATES.RED) {
       this.state = STATES.GREEN;
-      return;
     }
-    if (this.state === STATES.GREEN && commands[0] === '0') {
+    else if (this.state === STATES.GREEN && commands[0] === '0') {
       this.state = STATES.RED;
-      return;
     }
-    if (commands[0] === '0'){
-      this.state = STATES.BLUE;
-    } else {
+    else if (commands[0] === '1'){
       this.state = STATES.GREEN;
     }
   }
